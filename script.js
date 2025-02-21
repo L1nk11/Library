d36982135cd77878142cb24baa67c4b97c274025
Julio Oliveira <julio.oliveira06@outlook.com>
2025-02-21 15:21:27 -0300

    refactored script code

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-const overlay = document.getElementById('overlay')
-const addBook = document.getElementById('addBook')
-const popup = document.getElementById('popup')
+// book constructor
 
-addBook.addEventListener('click', function(){
+class Book {
+    constructor(tittle, author, pages, isRead) {
+        this.title = tittle
+        this.author = author
+        this.pages = pages
+        this.isRead = isRead
+    }
+}
+
+// Library
+
+
+
+// variables | references | buttons
+
+const Library = []
+
+const addBookButton = document.getElementById('addBook')
+const overlay = document.getElementById('overlay')
+const popUp = document.getElementById('popup')
+const bookForm = document.getElementById('bookForm')
+
+addBookButton.addEventListener('click', function() {
     overlay.style.display = 'block'
     popup.style.display = 'block'
 })
@@ -12,3 +32,27 @@ overlay.addEventListener('click', function(){
     popup.style.display = 'none'
 })
 
+// add book
+
+function receiveBookInput() {
+    const tittle = document.getElementById('tittle').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const read = document.getElementById('read').checked
+    return new Book(tittle, author, pages, read)
+}
+
+bookForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    const newBook = receiveBookInput()
+
+    Library.push(newBook)
+    overlay.style.display = 'none'
+    popup.style.display = 'none'
+    bookForm.reset()
+
+    // console.log(Library)
+})
+
+// create function to add new cell
